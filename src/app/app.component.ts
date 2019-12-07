0dbe6d1e85c79c3b776e4e5541f9fb2d3c9599bb
Brenden <brenden.niedermeyer@gmail.com>
2019-12-07 13:18:55 -0800

    remove begin component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { BeginService, ServerLessResponse } from './begin.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'begin-ng';
+  beginResponse: Observable<ServerLessResponse>;
+
+  constructor(private beginService: BeginService) {}
+
+  ngOnInit() {
+    this.beginResponse = this.beginService.fetchDataFromServerless();
+  }
 }
